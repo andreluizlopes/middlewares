c134fc9730c08016be38365cf0c6e34a340ee2f9
André Lopes <andre.lopes@dafiti.com.br>
2017-12-14 14:54:35 -0200

    middleware

---
+++ src/components/user_list.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class UserList extends Component {
+  componentWillMount() {
+    this.props.fetchUsers();
+  }
+
+  renderUser(user) {
+    return (
+      <div className="card card-block">
+        <h4 className="card-title">{user.name}</h4>
+        <p className="card-text">{user.company.name}</p>
+        <a className="btn btn-primary" target="_blank" href={`http://${user.website}`}>Website</a>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="user-list">
+        {this.props.users.map(this.renderUser)}
+      </div>
+    );
+  }
+}
+
+function mapStatesToProps(state) {
+  return { users: state.users };
+}
+
+export default connect(mapStatesToProps, actions)(UserList);
